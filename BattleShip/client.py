f2da4568eecc15b66b08504183dbc44669b4625a
John Trapp <johneyt54@gmail.com>
2016-09-19 21:00:53 -0600

    MidProjectCommit
    Much work done on client, now all that is needed is to save the file.

--- BattleShip/client.py
+++ BattleShip/client.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 
 
 def main():
@@ -6,7 +7,24 @@ def main():
     port = sys.argv[2]
     x = sys.argv[3]
     y = sys.argv[4]
-    print("\nSending (", x, ",", y, ") to IP Address ", ipadress, " using port ", port, ".\n")
+    url = ipadress + ':' + port
+    payload = {'x': x, 'y': y}
+    print("\nSending ", payload, " to IP Address ", url, ".\n")
+
+    r = requests.post(url, data=payload)
+    if r.reason == "OK":
+        hit(r.text)
+    if r.reason == 'Not Found':
+        print("Out of bounds! Server returned POST Reason \"Not Found\"")
+    if r.reason == "Gone":
+        print("Already fired upon! Server returned POST Reason \"Gone\"")
+    if r.reason == "Bad Request":
+        print("Message Not Formatted Correctly! Server returned POST Reason \"Bad Request\"")
+
+
+def hit(input):
+    payload = int(input)
+    #if payload == 1:
 
 
 main()
