64013ccf477aa25bb502246fd3fdc4f54040b2f8
John Trapp <johneyt54@gmail.com>
2016-09-20 01:39:55 -0600

    More client changes

--- BattleShip/client.py
+++ BattleShip/client.py
@@ -2,9 +2,18 @@ import sys
 import requests
 import re
 
-board = None
-x = None
-y = None
+board = [['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+         ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']]
+x = int
+y = int
 
 
 def main():
@@ -15,11 +24,13 @@ def main():
     x = sys.argv[3]
     y = sys.argv[4]
     url = "http://" + ipadress + ':' + port
-    payload = {'x': x, 'y': y}
+    payload = "x=" + x + "&y=" + y
     print("\nSending ", payload, " to IP Address ", url, ".\n")
 
     r = requests.post(url, data=payload)
     if r.reason == "OK":
+        print("Location Successful! Server returned POST Reason \"OK\"")
+        print(r.text)
         hit(r.text)
     if r.reason == 'Not Found':
         print("Out of bounds! Server returned POST Reason \"Not Found\"")
@@ -30,13 +41,17 @@ def main():
 
 
 def hit(binaryinput):
-    payload = int(binaryinput)
+    global x
+    global y
+    global board
+    xint = int(x)
+    yint = int(y)
     hit = int(re.search(r'\d+', binaryinput).group())
     loadfile()
     if hit == 1:
-        board[x][y] = 'X'
-    if hit == 2:
-        board[x][y] = '~'
+        board[xint][yint] = 'X'
+    if hit == 0:
+        board[xint][yint] = '~'
     if 'sink' in binaryinput:
         print("You sunk " + binaryinput[-1] + "!")
     closefile()
@@ -44,31 +59,33 @@ def hit(binaryinput):
 
 def loadfile():
     global board
-    with open("opponent_board.txt") as textFile:
-        read = textFile.read()
-        empty_check = textFile.read(1)
-        if not empty_check:
-            for x in range(0, 9):
-                for y in range(0, 9):
-                    board[x][y] = '_'
-        else:
-            n = 1
-            textFile.seek(0)
-            for x in range(0, 9):
-                for y in range(0, 9):
-                    board[x][y] = textFile.read(n)
+    with open("opponent_board.txt", 'r+') as textFile:
+        n = 0
+        textFile.seek(0)
+        for xl in range(0, 9):
+            for yl in range(0, 9):
+                if textFile.read(n) == '':
+                    board[xl][yl] = '_'
+                else:
+                    board[xl][yl] = textFile.read(n)
+                if yl == 9:
+                    n += 2
+                if yl != 9:
                     n += 1
+    textFile.close()
 
 
 def closefile():
     global board
-    global x
-    global y
+    with open("opponent_board.txt", 'r+') as textFile:
+        textFile.truncate()
+        textFile.close()
     with open("opponent_board.txt", 'w') as textFile:
-        for x in range(0, 9):
-            for y in range(0, 9):
-                textFile.write(board[x][y])
+        for xc in range(0, 9):
+            for yc in range(0, 9):
+                textFile.write(board[xc][yc])
             textFile.write("\n")
+        textFile.close()
 
 
 main()
